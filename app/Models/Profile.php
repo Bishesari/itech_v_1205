d486e70d60f73ea20a96219f275c970e122a1944
SiteD-Server <yasser.bishesari@gmail.com>
2025-08-15 18:05:17 +0330

    D - Server

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Profile extends Model
 {
     public $timestamps = false;
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
